45fe953b2f9286fc559e9f0067fbf9730a938a43
Breezes <92135861+BreezesxBreeezes@users.noreply.github.com>
2023-04-09 06:46:17 +0700

    responsive

--- tour-management/frontend/src/components/Header/Header.jsx
+++ tour-management/frontend/src/components/Header/Header.jsx
@@ -21,6 +21,7 @@ const nav__links = [
 const Header = () => {
 
   const headerRef = useRef(null)
+  const menuRef = useRef(null)
 
   const stickyHeaderFunc = () => {
     window.addEventListener('scroll', () => {
@@ -38,6 +39,8 @@ const Header = () => {
     return window.removeEventListener('scroll', stickyHeaderFunc);
   });
 
+  const toggleMenu = ()=> menuRef.current.classList.toggle('show__menu')
+
   return (
     <header className="header" ref={headerRef}>
       <Container>
@@ -50,7 +53,7 @@ const Header = () => {
             </div>
             {/* ============ logo end ============ */}
             {/* ============ menu start ============ */}
-            <div className="navigation">
+            <div className="navigation" ref={menuRef} onClick={toggleMenu}>
               <ul className="menu d-flex align-items-center gap-5">
                 {nav__links.map((item, index) => (
                   <li className="nav__item" key={index}>
@@ -74,7 +77,7 @@ const Header = () => {
                   <Link to='/register'>Register</Link>
                 </Button>
               </div>
-              <span className="mobile__menu">
+              <span className="mobile__menu" onClick={toggleMenu}>
                 <i class="ri-menu-line"></i>
               </span>
             </div>
