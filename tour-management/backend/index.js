14d86289af1dd8581162053054be6953d4518d4b
sang <dovansangabc123321@gmail.com>
2023-04-08 21:42:12 +0700

    "fix route api"

--- tour-management/backend/index.js
+++ tour-management/backend/index.js
@@ -12,6 +12,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
+const corsOptions = {
+    origin: true,
+    credentials: true
+};
+
 //database connection
 mongoose.set("strictQuery", false);
 const connect = async()=>{
@@ -30,11 +35,11 @@ const connect = async()=>{
 
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(cookieParser());
-app.use("/auth", authRoute);
-app.use("/tours", tourRoute);
-app.use("/users", userRoute);
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/tours", tourRoute);
+app.use("/api/v1/users", userRoute);
 
 app.listen(port, ()=>{
     connect();
--- tour-management/backend/routes/users.js
+++ tour-management/backend/routes/users.js
@@ -2,19 +2,21 @@ import express from 'express';
 import { deleteUser, getAllUser, getSingleUser, updateUser } from '../controllers/userController.js';
 const router = express.Router();
 
+import { verifyAdmin, verifyUser } from '../utils/verifyToken.js';
+
 // // create new user
 // router.post('/', creataTour);
 
 // update user
-router.put('/:id', updateUser);
+router.put('/:id', verifyUser, updateUser);
 
 // delete user
-router.delete('/:id', deleteUser);
+router.delete('/:id', verifyUser, deleteUser);
 
 // get single user
-router.get('/:id', getSingleUser);
+router.get('/:id', verifyUser, getSingleUser);
 
 // get all user
-router.get('/', getAllUser);
+router.get('/',verifyAdmin, getAllUser);
 
 export default router;
