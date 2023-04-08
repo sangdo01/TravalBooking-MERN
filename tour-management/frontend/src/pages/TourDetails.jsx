92097482144e8a886e16b36f9ce2ec0aa3c8e48b
Breezes <92135861+BreezesxBreeezes@users.noreply.github.com>
2023-04-08 22:04:00 +0700

    Tour page part 1

--- tour-management/frontend/src/pages/TourDetails.jsx
+++ tour-management/frontend/src/pages/TourDetails.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import '../styles/tour-details.css'
+import { Container, Row, Col, Form, ListGroup } from 'reactstrap'
+import { useParams } from 'react-router-dom'
+import tourData from '../assets/data/tours'
+import calculateAvgRating from '../utils/avgRating'
 
 const TourDetails = () => {
-  return (
-    <div>TourDetails</div>
-  )
+
+  const { id } = useParams();
+
+  //Call data from database  with call API method
+  const tour = tourData.find(tour => tour.id === id)
+
+  //destructure properties from tour object
+  const { photo, title, desc, price, address, reviews, city, distance, maxGroupSize } = tour
+
+  const { totalRating, avgRating } = calculateAvgRating(reviews)
+
+  return <>
+    <section>
+      <Container>
+        <Row>
+          <Col lg="8">
+            <div className="tour__content">
+              <img src={photo} alt="" />
+              <div className="tour__info">
+                <h2>{title}</h2>
+                <div className="d-flex align-items-center gap-5">
+                  <span className="tour__rating d-flex align-items-center gap-1">
+                    <i class="ri-star-fill" style={{ 'color': 'var(--secondary-color)' }}></i> {calculateAvgRating === 0 ? null : avgRating}
+                    {totalRating === 0 ? (
+                      "Not rated"
+                    ) : (
+                      <span>({reviews?.length})</span>
+                    )}
+                    <span><i class="ri-map-pin-user-line"></i>{address}</span>
+                  </span>
+                </div>
+                <div className="tour__extra-details">
+                  <span><i class="ri-map-pin-2-line"></i>{city}</span>
+                  <span><i class="ri-money-dollar-circle-line"></i>{price}</span>
+                  <span><i class="ri-group-line"></i>{maxGroupSize}</span>
+                </div>
+                <h5>Description</h5>
+                <p>{desc}</p>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  </>
 }
 
 export default TourDetails
