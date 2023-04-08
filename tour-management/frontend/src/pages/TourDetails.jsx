4483a3a18e85879a1d3e389868aa0631c29d8396
Breezes <92135861+BreezesxBreeezes@users.noreply.github.com>
2023-04-09 00:28:54 +0700

    Thank you page

--- tour-management/frontend/src/pages/TourDetails.jsx
+++ tour-management/frontend/src/pages/TourDetails.jsx
@@ -6,6 +6,7 @@ import tourData from '../assets/data/tours'
 import calculateAvgRating from '../utils/avgRating'
 import avatar from "../assets/images/avatar.jpg"
 import Booking from '../components/Booking/Booking'
+import Newsletter from '../shared/Newsletter'
 
 const TourDetails = () => {
 
@@ -125,6 +126,7 @@ const TourDetails = () => {
         </Row>
       </Container>
     </section>
+    <Newsletter />
   </>
 }
 
