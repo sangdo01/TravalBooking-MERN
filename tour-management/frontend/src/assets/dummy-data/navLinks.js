3950eaf6b7eb016132c180a3dce6778957523d6a
Breezes <92135861+BreezesxBreeezes@users.noreply.github.com>
2023-04-11 19:35:27 +0700

    admin site part 1

---
+++ tour-management/frontend/src/assets/dummy-data/navLinks.js
@@ -0,0 +1,24 @@
+const navLinks = [
+  {
+    path: "/admin/dashboard",
+    icon: "ri-apps-2-line",
+    display: "Dashboard",
+  },
+  {
+    path: "/admin/bookingslist",
+    icon: "ri-file-list-line",
+    display: "Booking List",
+  },
+  {
+    path: "/admin/listTour",
+    icon: "ri-file-list-fill",
+    display: "List Tour",
+  },
+  {
+    path: "/admin/addTours",
+    icon: "ri-add-line",
+    display: "Add Tour",
+  },
+];
+
+export default navLinks;
