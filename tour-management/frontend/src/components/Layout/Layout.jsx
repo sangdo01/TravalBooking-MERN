79427875a3b5a6d3eb8210c9e84a2f253cd7515c
Breezes <92135861+BreezesxBreeezes@users.noreply.github.com>
2023-04-11 19:44:46 +0700

    comit 1

--- tour-management/frontend/src/components/Layout/Layout.jsx
+++ tour-management/frontend/src/components/Layout/Layout.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import Header from './../Header/Header'
 import Routers from '../../router/Routers'
 import Footer from './../Footer/Footer'
+import { useLocation } from 'react-router-dom'
+import Slidebar from "../../Admin-site/Sidebar/Sidebar"
+import TopNav from '../../Admin-site/TopNav/TopNav'
 
 const Layout = () => {
-    return <>
-        <Header />
-        <Routers />
-        <Footer />
-    </>
+
+    const location = useLocation();
+    return (<>
+        {location.pathname.startsWith("/admin") ? <TopNav /> : <Header /> }
+        <div className='content'>
+            <Routers />
+        </div>
+
+        {location.pathname.startsWith("/admin") ? <Slidebar /> : <Footer /> }
+
+
+        {/* <Footer /> */}
+    </>)
 }
 
 export default Layout
